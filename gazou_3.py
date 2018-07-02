10e38a353b7e3d6b5182402799943b5b80622956
poooooooooooo <40170460+poooooooooooo@users.noreply.github.com>
2018-07-02 23:30:08 +0900

    Update gazou_3.py

--- gazou_3.py
+++ gazou_3.py
@@ -6,7 +6,17 @@ def nothing(x):
 
 cv2.namedWindow('image')
 
+cv2.createTrackbar('filt','image',1,10,nothing)
+
 cv2.createTrackbar('value','image',0,100,nothing)
+
+cv2.createTrackbar('R','image',0,200,nothing)
+cv2.createTrackbar('G','image',0,200,nothing)
+cv2.createTrackbar('B','image',0,200,nothing)
+
+cv2.setTrackbarPos('R','image',100)
+cv2.setTrackbarPos('G','image',100)
+cv2.setTrackbarPos('B','image',100)
 
 cap=cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
@@ -28,6 +38,17 @@ while(1):
     frame = cv2.LUT(frame,LUT)
     print(frame)
 
+    r = cv2.getTrackbarPos('R','image')/100
+    g = cv2.getTrackbarPos('G','image')/100
+    b = cv2.getTrackbarPos('B','image')/100
+
+    frame = frame/255
+    frame[:,:,2] *= r
+    frame[:,:,1] *= g
+    frame[:,:,0] *= b
+
+    v = cv2.getTrackbarPos('filt','image')
+    frame = cv2.GaussianBlur(frame,(25,25),v)
 
     cv2.imshow('image',frame)
     k=cv2.waitKey(1)&0xFF
